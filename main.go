926dcb7c305261f5abe701f8d9ee052f3f344ab0
maxmonkey950 <maxmonkey950@gmail.com>
2019-03-28 16:19:03 +0800

    add qa env

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, Kubernetes！I'm from Jenkins CI dev！")
+	fmt.Println("Hello, Kubernetes！I'm from Jenkins CI QA！")
 	fmt.Println("BRANCH:", os.Getenv("branch"))
 }
